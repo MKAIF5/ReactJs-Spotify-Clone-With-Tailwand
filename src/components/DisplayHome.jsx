40025c66ac601710e463219f7a455e3a92880566
mohammed kaif <mkaif555123@gmail.com>
2024-12-22 01:54:55 +0500

    Full Website 50% Work Completed

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
+import AlbumsItems from './AlbumsItems'
 
 const DisplayHome = () => {
     return (
         <>
             <Navbar />
-            <div>
-
-
+            <div className='mb-4'>
+                <h1 className='my-5 font-bold text-2xl'>Features Charts</h1>
+                <div className='flex overflow-auto'>
+                    {
+                        albumsData.map((val, i) => (
+                            <AlbumsItems
+                                key={i}
+                                name={val.name}
+                                desc={val.desc}
+                                id={val.id}
+                                image={val.image}
+                            />
+                        ))}
+                </div>
             </div>
         </>
     )
